44548eab28f6092bb2e2cc238670a65e598a7460
imransarkerweb <imransarker.web@gmail.com>
2023-05-02 21:33:50 +0600

    cors added

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
-const port = process.ENV.PORT || 5000;
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+const chefs = require("./data/chef-data.json");
 
 app.get("/", (req, res) => {
   res.send("Node is running");
 });
 
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
+});
 app.listen(port, () => {
   console.log(`App is running at port ${port}`);
 });
