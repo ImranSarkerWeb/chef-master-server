5a2a96e822cacb8f07ee3c64d7b428442644a2ce
imransarkerweb <imransarker.web@gmail.com>
2023-05-03 07:49:40 +0600

    question data added

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 const chefs = require("./data/chef-data.json");
 const chefsRecipes = require("./data/chef-with-recipe.json");
+const category = require("./data/category.json");
+const questions = require("./data/blog-questions.json");
 
 app.get("/", (req, res) => {
   res.send("Node is running");
@@ -21,6 +23,13 @@ app.get("/recipes/:id", (req, res) => {
 
   const recipe = chefsRecipes.find((chefRecipe) => chefRecipe.id == id);
   res.send(recipe);
+});
+
+app.get("/catergory", (req, res) => {
+  res.send(category);
+});
+app.get("/questions", (req, res) => {
+  res.send(questions);
 });
 app.listen(port, () => {
   console.log(`App is running at port ${port}`);
